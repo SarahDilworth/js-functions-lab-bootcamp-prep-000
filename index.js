28a39e725f904b166ebbd156f6e78532de22700c
Sarah Dilworth <sarah.petty18@gmail.com>
2018-05-15 16:18:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,8 @@ function happyHolidaysTo() {
 return `Happy holidays, ${name}!`
 }
 function happyHolidayTo() {
+return `Happy ${holiday}, ${name}!`
+}
+function holidayCountdown() {
+return `It\'s ${days} days until ${holiday}!`
+}
